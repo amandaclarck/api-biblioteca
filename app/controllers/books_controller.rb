0d25cc5b59626a61dc2c2a60d21de4d2b5cf1f3a
Amanda Pedroso <amanda.pedroso@hotmail.com>
2019-07-15 06:46:49 -0300

    Primeiro commit - Adiciona várias pastas etc

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,26 @@
+class BooksController < ApplicationController
+	def index
+		render json: { status: 200, data: Book.all }, status: :ok
+	end
+	
+	def create
+		#protect_from_forgery with: :null_session
+		@book = Book.create(book_params)
+		
+		if @book.save
+			render json: { status: 201, message: 'book created!', data: @book }, status: :created
+		else
+			render json: { status: 422, message: 'book not created', data: @book.errors }, status: :unprocessable_entity
+		end
+	end
+	
+	def show
+		@book = Book.find(params[:id])
+		render json: { status: 200, data: @book }, status: :ok
+	end
+	
+	private
+	def book_params
+		params.require(:book).permit(:title, :price, :genre)
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'books', action: :index, to: 'books#index'
+  post 'create', action: :create, to: 'books#create'
+  get 'show/:id', action: :show, to: 'books#show'
+end
